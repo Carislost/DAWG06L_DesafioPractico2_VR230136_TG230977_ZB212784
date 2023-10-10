99c576094787b4370f0b1b73498d131b1631ae07
Needhelpy <141784734+Needhelpy@users.noreply.github.com>
2023-10-09 19:07:13 -0600

    lista en appform
    subo una version de el programa con la lista de los paises ya completa

--- DAWG06L_DesafioPractico2_VR230136_TG230977_ZB212784/appform.js
+++ DAWG06L_DesafioPractico2_VR230136_TG230977_ZB212784/appform.js
@@ -13,20 +13,54 @@ class Paciente {
 }
 
 const departamentosMunicipios = {
-    "Ahuachapán": ["Ahuachapán", "Atiquizaya", "Jujutla"],
-    "Santa Ana": ["Santa Ana", "Chalchuapa", "Metapán"],
-    "Sonsonate": ["Sonsonate", "Acajutla", "Nahuizalco"],
-    "Chalatenango": ["Chalatenango", "Dulce Nombre de María", "El Paraíso"],
-    "La Libertad": ["Santa Tecla", "Antiguo Cuscatlán", "Quezaltepeque"],
-    "San Salvador": ["San Salvador", "Mejicanos", "Soyapango"],
-    "Cuscatlán": ["Cojutepeque", "Suchitoto", "San Pedro Perulapán"],
-    "La Paz": ["Zacatecoluca", "San Pedro Masahuat", "Olocuilta"],
-    "Cabañas": ["Sensuntepeque", "Ilobasco", "Victoria"],
-    "San Vicente": ["San Vicente", "Tecoluca", "Guadalupe"],
-    "Usulután": ["Usulután", "Jiquilisco", "Berlin"],
-    "San Miguel": ["San Miguel", "Chapeltique", "Chinameca"],
-    "Morazán": ["San Francisco Gotera", "Osicala", "Cacaopera"],
-    "La Unión": ["La Unión", "Santa Rosa de Lima", "El Sauce"]
+
+    "Ahuachapán": [ "Ahuachapán", "Apaneca", "Atiquizaya", "Concepción de Ataco", "El Refugio", "Guaymango", "Jujutla", "San Francisco Menéndez", "San Lorenzo", 
+        "San Pedro Puxtla", "Tacuba", "Turín" ],
+
+    "Santa Ana": ["Santa Ana", "Candelaria de la Frontera", "Chalchuapa", "Coatepeque", "El Congo", "El Porvenir", "Masahuat", "Metapán", "San Antonio Pajonal", 
+        "San Sebastián Salitrillo", "Santa Rosa Guachipilín", "Santiago de la Frontera", "Texistepeque" ],
+
+    "Sonsonate": [ "Sonsonate", "Acajutla", "Armenia", "Caluco", "Cuisnahuat", "Izalco", "Juayúa", "Nahuizalco", "Nahulingo", "Salcoatitán", "San Antonio del Monte", 
+        "San Julián", "Santa Catarina Masahuat", "Santa Isabel Ishuatán", "Santo Domingo de Guzmán", "Sonzacate" ],
+
+    "Chalatenango": [ "Chalatenango", "Agua Caliente", "Arcatao", "Azacualpa", "Citalá", "Comalapa", "Concepción Quezaltepeque", "Dulce Nombre de María", "El Carrizal", 
+        "El Paraíso", "La Laguna", "La Palma", "La Reina", "Las Vueltas", "Nombre de Jesús", "Nueva Concepción", "Nueva Trinidad", "Ojos de Agua", "Potonico", 
+        "San Antonio de la Cruz", "San Antonio Los Ranchos", "San Fernando", "San Francisco Lempa", "San Francisco Morazán", "San Ignacio", "San Isidro Labrador", 
+        "San José Cancasque", "San José Las Flores", "San Luis del Carmen", "San Miguel de Mercedes", "San Rafael", "Santa Rita", "Tejutla", "Tenancingo" ],
+
+    "La Libertad": [ "Santa Tecla", "Antiguo Cuscatlán", "Chiltiupán", "Ciudad Arce", "Colón", "Comasagua", "Huizúcar", "Jayaque", "Jicalapa", "La Libertad", 
+        "Nuevo Cuscatlán", "Opico", "Quezaltepeque", "Sacacoyo", "San José Villanueva", "San Matías", "San Pablo Tacachico", "Talnique", "Tamanique", "Teotepeque", 
+        "Tepecoyo", "Zaragoza" ],
+
+    "San Salvador": [ "San Salvador", "Aguilares", "Apopa", "Ayutuxtepeque", "Cuscatancingo", "Delgado", "El Paisnal", "Guazapa", "Ilopango", "Mejicanos", "Nejapa", 
+        "Panchimalco", "Rosario de Mora", "San Marcos", "San Martín", "Santiago Texacuangos", "Santo Tomás", "Soyapango", "Tonacatepeque" ],
+
+    "Cuscatlán": [ "Cojutepeque", "Candelaria", "El Carmen", "El Rosario", "Monte San Juan", "Oratorio de Concepción", "San Bartolomé Perulapía", "San Cristóbal", 
+        "San José Guayabal", "San Pedro Perulapán", "San Rafael Cedros", "San Ramón", "Santa Cruz Analquito", "Santa Cruz Michapa", "Suchitoto", "Tenancingo" ],
+    
+    "La Paz": [ "Zacatecoluca", "Cuyultitán", "El Rosario", "Jerusalén", "Mercedes La Ceiba", "Olocuilta", "Paraíso de Osorio", "San Antonio Masahuat", "San Emigdio", 
+        "San Francisco Chinameca", "San Juan Nonualco", "San Juan Talpa", "San Juan Tepezontes", "San Luis La Herradura", "San Luis Talpa", "San Miguel Tepezontes", 
+        "San Pedro Masahuat", "San Pedro Nonualco", "San Rafael Obrajuelo", "Santa María Ostuma", "Santiago Nonualco", "Tapalhuaca" ],
+
+    "Cabañas": [ "Sensuntepeque", "Cinquera", "Dolores", "Guacotecti", "Ilobasco", "Jutiapa", "San Isidro", "Tejutepeque", "Victoria", "San Sebastián", "Guadalupe"],
+
+    "San Vicente": [ "San Vicente", "Apastepeque", "Guadalupe", "San Cayetano Istepeque", "San Esteban Catarina", "San Ildefonso", "San Lorenzo", "San Sebastián", 
+        "Santa Clara", "Santo Domingo", "Tecoluca", "Tepetitán", "Verapaz" ],
+
+    "Usulután": [ "Usulután", "Alegría", "Berlín", "California", "Concepción Batres", "El Triunfo", "Ereguayquín", "Estanzuelas", "Jiquilisco", "Jucuapa", "Jucuarán", 
+        "Mercedes Umaña", "Nueva Granada", "Ozatlán", "Puerto El Triunfo", "San Agustín", "San Buenaventura", "San Dionisio", "San Francisco Javier", "Santa Elena", 
+        "Santa María", "Santiago de María", "Tecapán" ],
+
+    "San Miguel": [ "San Miguel", "Carolina", "Chapeltique", "Chinameca", "Chirilagua", "Ciudad Barrios", "Comacarán", "El Tránsito", "Lolotique", "Moncagua", 
+        "Nueva Guadalupe", "Nuevo Edén de San Juan", "Quelepa", "San Antonio", "San Gerardo", "San Jorge", "San Luis de la Reina", "San Rafael Oriente", "Sesori", 
+        "Uluazapa" ],
+
+    "Morazán":[ "San Francisco Gotera", "Arambala", "Cacaopera", "Chilanga", "Corinto", "Delicias de Concepción", "El Divisadero", "El Rosario", "Gualococti", 
+        "Guatajiagua", "Joateca", "Jocoaitique", "Jocoro", "Lolotiquillo", "Meanguera", "Osicala", "Perquín", "San Carlos", "San Fernando", "San Isidro", "San Simón",
+        "Sensembra", "Sociedad", "Torola", "Yamabal", "Yoloaiquín" ],
+    
+    "La Unión": [ "La Unión", "Anamorós", "Bolívar", "Concepción de Oriente", "Conchagua", "El Carmen", "El Sauce", "Intipucá", "Lislique", "Meanguera del Golfo", 
+        "Nueva Esparta", "Pasaquina", "Polorós", "San Alejo", "San José", "Santa Rosa de Lima", "Yayantique", "Yucuaiquín" ]
 };
 
 function actualizarMunicipios() {
